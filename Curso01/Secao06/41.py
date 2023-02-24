4ea90e4a06ed74e836fa06defe19efd4c8b54714
CleitonSilvaPaes <cleiton03122000@gmail.com>
2023-02-24 13:26:28 -0300

    exer 41

--- Curso01/Secao06/41.py
+++ Curso01/Secao06/41.py
@@ -3,6 +3,11 @@ while rodando:
     r1 = float(input('R1: '))
     r2 = float(input('R2: '))
 
-    calc = (r1 * r2)/(r1+r2)
+    calc = 0
     
     if calc != 0:
+        calc = (r1 * r2)/(r1+r2)
+        print(f'R: {calc}')
+    else:
+        print(f'R: {calc}')
+        rodando = False
