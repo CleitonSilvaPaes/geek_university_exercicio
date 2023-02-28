c3e0f1cbc9a0b11293ed292aac5adb9bf8bbbd8c
CleitonSilvaPaes <cleiton03122000@gmail.com>
2023-02-28 19:11:31 -0300

    Deixando mais legivel e adicionando permicao de pdf

--- Curso01/Secao07/35arru.py
+++ Curso01/Secao07/35arru.py
@@ -3,7 +3,7 @@ soma = []
 x = int(input('Digite numero X: '))
 y = int(input('Digite numero Y: '))
 
-if (x >=0 and x <= 10000) and (y >= 0 and y <= 10000):
+if (x >= 0 and x <= 10000) and (y >= 0 and y <= 10000):
     if x > y:
         x, y = y, x
     for i in range(x, y+1):
@@ -11,7 +11,7 @@ if (x >=0 and x <= 10000) and (y >= 0 and y <= 10000):
     for i in range(len(vet1)):
         tmp = vet1[i] + vet1[i-1]
         soma.append(tmp)
-    print(f'Vetor: ', *vet1)
-    print(f'Soma dos algariomos: ', *soma)
+    print('Vetor: ', *vet1)
+    print('Soma dos algariomos: ', *soma)
 else:
     print('Valor Invalido')
