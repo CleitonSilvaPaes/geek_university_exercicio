c8b5706ddd7a27abbd60c713e661354f436f7db5
CleitonSilvaPaes <cleiton03122000@gmail.com>
2023-03-01 13:11:28 -0300

    finalizado 16p2

---
+++ Curso01/Secao07/16p2.py
@@ -0,0 +1,34 @@
+from random import randrange
+
+# Montando o gabarito
+gabarito = [['a', 'b', 'c', 'd', 'e'][randrange(5)]for i in range(10)]
+
+# Adicionado alunos com suas notas e matriculas
+alunos = {}
+for i in range(3):
+    alunos[i] = {
+        'resp': [['a', 'b', 'c', 'd', 'e'][randrange(5)]for i in range(10)]
+    }
+
+
+# Verificar as resposta e atribuir nota
+for chave, valor in alunos.items():
+    nota = 0
+    for resp in valor.values():
+        for i in range(len(resp)):
+            if gabarito[i] == resp[i]:
+                nota += 1
+    if nota >= 7:
+        alunos[chave]['apt'] = True
+    else:
+        alunos[chave]['apt'] = False
+    alunos[chave]['nota'] = nota
+
+# Verifica as nota
+for chave, valor in alunos.items():
+    print(f'Matricula: {chave}')
+    print('Respostas: ', *alunos[chave]['resp'])
+    print(f'Nota: {alunos[chave]["nota"]}')
+    print('Aprovado' if alunos[chave]["apt"] else 'Reprovado')
+    print('Gabarito:  ', *gabarito)
+    print()
