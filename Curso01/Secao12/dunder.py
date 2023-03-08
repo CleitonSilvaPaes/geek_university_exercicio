46d43ffe441958cd12abde138cfb4b8f9f08645b
CleitonSilvaPaes <cleiton03122000@gmail.com>
2023-03-08 13:12:19 -0300

    dunder

---
+++ Curso01/Secao12/dunder.py
@@ -0,0 +1,23 @@
+"""
+Dunder Name e Dunder Main
+
+Dunder Name -> __name__
+
+Dunder Main -> __main__
+
+Em Python, sao utilizados dunder para criar funcoes, atributos, propriedades e ect utilizando Double Under para nao gerar conflito com os nomes desses elementos na programacao.
+
+# Na linguagem C, temos um programa da seguite forma:
+
+int main(){
+    return 0;
+}
+
+# Na linguagem Java, temos um programa de seguinte forma:
+
+public static void main(String[] args){
+    
+}
+
+# Em Python, se executarmos um modulo Python diretamente na linha de comando, internamente o Python atribuira a variavel __name__ o valor __main__ indicando que este modulo é o modulo de execucao principal
+"""
